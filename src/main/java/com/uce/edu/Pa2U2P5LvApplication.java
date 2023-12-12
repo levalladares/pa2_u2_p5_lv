de6ea4b6f7d95d67cbb62c8367b94c41be2a2b32
Luis <levalladares@uce.edu.ec>
2023-12-11 19:43:24 -0500

    Taller14

---
+++ src/main/java/com/uce/edu/Pa2U2P5LvApplication.java
@@ -0,0 +1,20 @@
+package com.uce.edu;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pa2U2P5LvApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pa2U2P5LvApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
